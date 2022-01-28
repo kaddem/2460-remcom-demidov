cbe5cb4acd023fcd25da5ecf56ea3f00af9b6d68
Kirill Demidov <kademidov@gmail.com>
2022-01-29 01:38:27 +0300

    аккордионы, мобильное меню

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,31 @@
-console.log('script work!');
+$(document).ready(function(){
+
+  // let isOpen = false;
+
+  $('.j-burger').on('click', function(){
+    $('.j-menu').slideToggle();
+    // if (isOpen) {
+    //   $('.j-menu').slideUp();
+    //   isOpen = false;
+    // } else {
+    //   $('.j-menu').slideDown();
+    //   isOpen = true;
+    // }
+  });
+
+
+  // аккордион
+  let prevBtn;
+
+  $('.j-accordion-btn').on('click', function(){
+    if (this === prevBtn) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.j-accordion-btn').next().slideUp();
+    $(this).next().slideToggle();
+    prevBtn = this;
+  });
+
+});
