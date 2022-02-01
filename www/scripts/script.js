fa6a5906b43ac2ed84d721b442be59b38a5241b6
Kirill Demidov <kademidov@gmail.com>
2022-02-01 13:37:38 +0300

    slick; фильтрация и табы; верстка отзывов

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -28,4 +28,49 @@ $(document).ready(function(){
     prevBtn = this;
   });
 
+
+  // Табы в контактах
+  $('.j-tabs-link').on('click', function(){
+    const index = $(this).index('.j-tabs-link');
+
+    $('.j-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    $('.j-tabs-content').removeClass('active');
+    $('.j-tabs-content').eq(index).addClass('active');
+  });
+
+
+  // Фильтр портфолио
+  $('.j-filter-link').on('click', function(){
+
+    const filterType = $(this).data('filter');
+
+    $('.j-filter-link').removeClass('active');
+    $(this).addClass('active');
+
+    if(filterType === 'all') {
+      $('.j-case').show();
+      return;
+    }
+
+    $('.j-case').each(function() {
+      const caseType = $(this).data('type');
+
+      if (filterType === caseType) {
+        $(this).show();
+        return;
+      }
+
+      $(this).hide();
+    });
+  });
+
+
+  // Slider
+
+  $('.j-slider').slick({
+    dots: true,
+  });
+
 });
